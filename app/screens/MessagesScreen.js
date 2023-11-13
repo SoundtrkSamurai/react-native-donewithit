6784e6c740be589fa783fc927471583c01b0a27a
SoundtrkSamurai <furtimat3@yahoo.com>
2023-11-13 11:21:47 -0800

    lists: Create a component for rendering messages

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,43 @@
+import Constants from 'expo-constants'
+import { FlatList, StyleSheet, Text } from 'react-native'
+import ListItem from '../components/ListItem'
+import Screen from '../components/Screen'
+
+const messages = [
+  {
+    id: 1,
+    title: 'T1',
+    description: 'D1',
+    image: require('../assets/avatar.png')
+  },
+  {
+    id: 2,
+    title: 'T2',
+    description: 'D2',
+    image: require('../assets/avatar.png')
+  },
+]
+const MessagesScreen = () => {
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={message => message.id.toString()}
+        renderItem={({ item }) =>
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+          />
+        }
+      >
+        <Text>MessagesScreen</Text>
+      </FlatList>
+    </Screen>
+  )
+}
+
+export default MessagesScreen
+
+const styles = StyleSheet.create({
+})
