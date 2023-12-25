d5e6bd947ce2e46636f87132e71f0a0d57751060
SoundtrkSamurai <furtimat3@yahoo.com>
2023-12-24 16:26:21 -0800

    improvements: refactor picker components

--- app/components/forms/AppFormPicker.js
+++ app/components/forms/AppFormPicker.js
@@ -3,14 +3,16 @@ import { useFormikContext } from "formik";
 import AppPicker from "../AppPicker";
 import ErrorMessage from "./ErrorMessage";
 
-const AppFormPicker = ({ items, width, name, placeholder }) => {
+const AppFormPicker = ({ items, name, numberOfColumns, PickerItemComponent, placeholder, width }) => {
   const { errors, setFieldValue, touched, values } = useFormikContext();
 
   return (
     <>
       <AppPicker
         items={items}
+        numberOfColumns={numberOfColumns}
         onSelectItem={(item) => setFieldValue(name, item)}
+        PickerItemComponent={PickerItemComponent}
         placeholder={placeholder}
         selectedItem={values[name]}
         width={width}
