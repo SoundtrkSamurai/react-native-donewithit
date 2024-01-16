cbfc7ac915e2b09685a18d606bb06f7f13bb5ab3
SoundtrkSamurai <furtimat3@yahoo.com>
2024-01-15 23:06:27 -0800

    navigation: update temp current state of App.js

--- App.js
+++ App.js
@@ -1,17 +1,76 @@
-import { useState } from 'react';
+import 'react-native-gesture-handler';
+import { Button, Text, Platform } from 'react-native';
 import Screen from './app/components/Screen';
-import LoginScreen from './app/screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-const categories = [
-  { label: "Funiture", value: "1"},
-  { label: "Clothing", value: "2"},
-  { label: "Cameras", value: "3"},
-];
+const Tweets = ({ navigation }) => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Button
+      title='View Tweet'
+      onPress={() => {
+        navigation.navigate('TweetDetails', { id: 1 });
+      }}
+    />
+  </Screen>
+);
+
+const TweetDetails = ({ route }) => (
+  <Screen>
+    <Text>Tweet Details {route.params.id}</Text>
+  </Screen>
+);
+
+const Stack = createStackNavigator();
+const FeedNavigator = () => (
+  <Stack.Navigator
+    initialRouteName='Tweets'
+    screenOptions={{
+      headerStyle: { backgroundColor: 'dodgerblue' },
+      headerTintColor: 'white',
+    }}
+  >
+    <Stack.Screen name='TweetDetails' component={TweetDetails} />
+    <Stack.Screen name='Tweets' component={Tweets} />
+  </Stack.Navigator>
+);
+
+const Account = () => (
+  <Screen>
+    <Text>Account</Text>
+  </Screen>
+);
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator
+    screenOptions={{
+      tabBarActiveBackgroundColor: 'tomato',
+      tabBarActiveTintColor: 'white',
+      tabBarInactiveBackgroundColor: '#eee',
+      tabBarInactiveTintColor: 'black',
+    }}
+  >
+    <Tab.Screen
+      name='Feed'
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ size, color }) => (
+          <MaterialCommunityIcons name='home' size={size} color={color} />
+        ),
+      }}
+    />
+    <Tab.Screen name='Account' component={Account} />
+  </Tab.Navigator>
+);
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
   return (
-    <LoginScreen />
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
   );
 }
