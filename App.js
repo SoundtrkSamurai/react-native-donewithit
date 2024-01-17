8d3f092a20773037b83c390dd81005466371e1c0
SoundtrkSamurai <furtimat3@yahoo.com>
2024-01-16 18:31:02 -0800

    navigation: wire up auth nav to app.js

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import 'react-native-gesture-handler';
-import { Button, Text, Platform } from 'react-native';
-import Screen from './app/components/Screen';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
+import "react-native-gesture-handler";
+import { Button, Text, Platform } from "react-native";
+import Screen from "./app/components/Screen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import AuthNavigator from "./app/nagivation/AuthNavigator";
 
 const Tweets = ({ navigation }) => (
   <Screen>
     <Text>Tweets</Text>
     <Button
-      title='View Tweet'
+      title="View Tweet"
       onPress={() => {
-        navigation.navigate('TweetDetails', { id: 1 });
+        navigation.navigate("TweetDetails", { id: 1 });
       }}
     />
   </Screen>
@@ -28,14 +29,14 @@ const TweetDetails = ({ route }) => (
 const Stack = createStackNavigator();
 const FeedNavigator = () => (
   <Stack.Navigator
-    initialRouteName='Tweets'
+    initialRouteName="Tweets"
     screenOptions={{
-      headerStyle: { backgroundColor: 'dodgerblue' },
-      headerTintColor: 'white',
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
     }}
   >
-    <Stack.Screen name='TweetDetails' component={TweetDetails} />
-    <Stack.Screen name='Tweets' component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    <Stack.Screen name="Tweets" component={Tweets} />
   </Stack.Navigator>
 );
 
@@ -48,29 +49,29 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => (
   <Tab.Navigator
     screenOptions={{
-      tabBarActiveBackgroundColor: 'tomato',
-      tabBarActiveTintColor: 'white',
-      tabBarInactiveBackgroundColor: '#eee',
-      tabBarInactiveTintColor: 'black',
+      tabBarActiveBackgroundColor: "tomato",
+      tabBarActiveTintColor: "white",
+      tabBarInactiveBackgroundColor: "#eee",
+      tabBarInactiveTintColor: "black",
     }}
   >
     <Tab.Screen
-      name='Feed'
+      name="Feed"
       component={FeedNavigator}
       options={{
         tabBarIcon: ({ size, color }) => (
-          <MaterialCommunityIcons name='home' size={size} color={color} />
+          <MaterialCommunityIcons name="home" size={size} color={color} />
         ),
       }}
     />
-    <Tab.Screen name='Account' component={Account} />
+    <Tab.Screen name="Account" component={Account} />
   </Tab.Navigator>
 );
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
