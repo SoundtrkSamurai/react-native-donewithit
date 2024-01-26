8b1e192e700c7ef0a9b3313628a968f1557f21f3
SoundtrkSamurai <furtimat3@yahoo.com>
2024-01-25 22:52:46 -0800

    network: Connect to listings endpoint

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,40 +1,54 @@
+import { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
+import listingsApi from "../api/listings";
+import Button from "../components/AppButton";
+import AppText from "../components/AppText";
 import Card from "../components/Card";
 import Screen from "../components/Screen";
+import routes from "../nagivation/routes";
 import colors from "../utils/color";
 
 const { light } = colors;
 
-const listings = [
-  {
-    id: 1,
-    title: "Red Jacket for sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "Couch in great condition",
-    price: 1000,
-    image: require("../assets/couch.jpg"),
-  },
-];
-
 const ListingsScreen = ({ navigation }) => {
+  const [listings, setListings] = useState([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const { problem, data } = await listingsApi.getListings();
+    if (problem) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    setListings(data);
+  };
+
   return (
     <Screen style={styles.screen}>
-      <FlatList
-        data={listings}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={`\$${item.price}`}
-            image={item.image}
-            onPress={() => navigation.navigate("ListingDetails", item)}
-          />
-        )}
-      />
+      {error ? (
+        <>
+          <AppText>Couldn't Retrieve the Listings</AppText>
+          <Button text="Retry" onPress={loadListings} />
+        </>
+      ) : (
+        <FlatList
+          data={listings}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={`\$${item.price}`}
+              imageURL={item.images[0].url}
+              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+            />
+          )}
+        />
+      )}
     </Screen>
   );
 };
