c679265eb7f5a5cbf761eaf16894268721435604
SoundtrkSamurai <furtimat3@yahoo.com>
2024-01-29 23:55:43 -0800

    network: Add lottie loading animation

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,54 +1,60 @@
-import { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
-import listingsApi from "../api/listings";
-import Button from "../components/AppButton";
+import { useEffect, useState } from "react";
+import ActivityIndicator from "../components/ActivityIndicator";
 import AppText from "../components/AppText";
+import Button from "../components/AppButton";
 import Card from "../components/Card";
-import Screen from "../components/Screen";
-import routes from "../nagivation/routes";
 import colors from "../utils/color";
+import listingsApi from "../api/listings";
+import routes from "../nagivation/routes";
+import Screen from "../components/Screen";
 
 const { light } = colors;
 
 const ListingsScreen = ({ navigation }) => {
   const [listings, setListings] = useState([]);
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     loadListings();
   }, []);
 
   const loadListings = async () => {
+    setLoading(true);
     const { ok, data } = await listingsApi.getListings();
+    setLoading(false);
+
     if (!ok) {
       setError(true);
       return;
     }
+
     setError(false);
     setListings(data);
   };
 
   return (
     <Screen style={styles.screen}>
-      {error ? (
+      {error && (
         <>
           <AppText>Couldn't Retrieve the Listings</AppText>
           <Button text="Retry" onPress={loadListings} />
         </>
-      ) : (
-        <FlatList
-          data={listings}
-          keyExtractor={(listing) => listing.id.toString()}
-          renderItem={({ item }) => (
-            <Card
-              title={item.title}
-              subTitle={`\$${item.price}`}
-              imageURL={item.images[0].url}
-              onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            />
-          )}
-        />
       )}
+      <ActivityIndicator visible={true} size="large" />
+      {/* <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={`\$${item.price}`}
+            imageURL={item.images[0].url}
+            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
+          />
+        )}
+      /> */}
     </Screen>
   );
 };
