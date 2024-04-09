ebf8d658a605289f16cc18dbc8ede33971e134a0
SoundtrkSamurai <furtimat3@yahoo.com>
2024-04-09 09:15:35 -0700

    auth: Correct users api

--- app/api/users.js
+++ app/api/users.js
@@ -2,9 +2,7 @@ import client from "./client";
 
 const endpoint = "/users";
 
-const register = (userInfo) => {
-  client.post(endpoint, userInfo);
-};
+const register = (userInfo) => client.post(endpoint, userInfo);
 
 export default {
   register,
