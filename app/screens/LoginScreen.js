6d20bddff9cc4da487b963e0889e9d05476bc15f
SoundtrkSamurai <furtimat3@yahoo.com>
2024-04-08 18:42:03 -0700

    auth: Wireup up Auth to LoginScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,68 +1,89 @@
-import { Formik } from 'formik'
-import { Image, StyleSheet } from 'react-native'
-import * as Yup from 'yup'
-import AppButton from '../components/AppButton'
-import AppTextInput from '../components/AppTextInput'
-import ErrorMessage from '../components/ErrorMessage'
-import Screen from '../components/Screen'
+import "core-js/stable/atob";
+import { Formik } from "formik";
+import { Image, StyleSheet } from "react-native";
+import { jwtDecode } from "jwt-decode";
+import { useState, useContext } from "react";
+import * as Yup from "yup";
+import AppButton from "../components/AppButton";
+import AppTextInput from "../components/AppTextInput";
+import authApi from "../api/auth";
+import AuthContext from "../auth/context";
+import authStore from "../auth/storage";
+import ErrorMessage from "../components/forms/ErrorMessage";
+import Screen from "../components/Screen";
 
 const validationSchema = Yup.object().shape({
-  email: Yup.string().required().email().label('Email'),
-  password: Yup.string().required().min(4).label('Password')
-})
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 const LoginScreen = () => {
-  return ( 
-   <Screen style={styles.container}>
-    <Image 
-      style={styles.logo}
-      source={require('../assets/logo-red.png')} 
-    />
+  const authContext = useContext(AuthContext);
+  const [loginFailed, setLoginFailed] = useState(false);
 
-    <Formik
-      initialValues={{email: '', password: ''}}
-      onSubmit={values => console.log(values)}
-      validationSchema={validationSchema}
-    >
-      {({ handleChange, handleSubmit, errors }) => (
-        <>
-          <AppTextInput
-            autoCapitalize='none'
-            autoCorrect={false}
-            icon='email'
-            keyboardType='email-address'
-            onChangeText={handleChange('email')}
-            placeholder='Email'
-          />
-          <ErrorMessage error={errors.email} />
-          <AppTextInput 
-            autoCapitalize='none'
-            autoCorrect={false}
-            icon='lock'
-            onChangeText={handleChange('password')}
-            placeholder='Password'
-            secureTextEntry={true}
-          />
-          <ErrorMessage error={errors.password} />
-          <AppButton text='Login' onPress={handleSubmit} />
-        </>
-      )}
-    </Formik>
-   </Screen>
-  )
-}
+  const handleSubmit = async ({ email, password }) => {
+    const result = await authApi.login(email, password);
+    if (!result.ok) {
+      setLoginFailed(true);
+      return;
+    }
+    setLoginFailed(false);
+    const user = jwtDecode(result.data);
+    authContext.setUser(user);
+    authStore.storeToken(result.data);
+  };
+  return (
+    <Screen style={styles.container}>
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
 
-export default LoginScreen
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ handleChange, handleSubmit, errors }) => (
+          <>
+            <ErrorMessage
+              error="Invalid email and/or password"
+              visible={loginFailed}
+            />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="email"
+              keyboardType="email-address"
+              onChangeText={handleChange("email")}
+              placeholder="Email"
+            />
+            <ErrorMessage error={errors.email} />
+            <AppTextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              icon="lock"
+              onChangeText={handleChange("password")}
+              placeholder="Password"
+              secureTextEntry={true}
+            />
+            <ErrorMessage error={errors.password} />
+            <AppButton text="Login" onPress={handleSubmit} />
+          </>
+        )}
+      </Formik>
+    </Screen>
+  );
+};
+
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-  container: { 
-    padding: 10
+  container: {
+    padding: 10,
   },
   logo: {
     width: 80,
     height: 80,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginTop: 50,
-    marginBottom: 20
-  }
+    marginBottom: 20,
+  },
 });
