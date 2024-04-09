4d6815a35076591fb99df88d315bbb6623aa028d
SoundtrkSamurai <furtimat3@yahoo.com>
2024-04-08 23:38:17 -0700

    auth: Refactor RegisterScreen

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -1,27 +1,60 @@
 import { Formik } from "formik";
 import { StyleSheet } from "react-native";
+import usersApi from "../api/users";
+import { useState } from "react";
 import * as Yup from "yup";
+import ActivityIndicator from "../components/ActivityIndicator";
 import AppButton from "../components/AppButton";
 import AppTextInput from "../components/AppTextInput";
+import authApi from "../api/auth";
 import ErrorMessage from "../components/forms/ErrorMessage";
 import Screen from "../components/Screen";
+import useApi from "../hooks/useApi";
+import useAuth from "../hooks/useAuth";
+import AppActivityIndicator from "../components/AppActivityIndicator";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(4).label("Password"),
 });
+
 const RegisterScreen = () => {
-  const initialValues = { name: "", email: "", password: "" };
+  const registerApi = useApi(usersApi.register);
+  const loginApi = useApi(authApi.login);
+  const auth = useAuth();
+  const [error, setError] = useState();
+
+  const handleSubmit = async (userInfo) => {
+    const result = await registerApi.request(userInfo);
+
+    if (!result.ok) {
+      if (result.data) setError(result.data.error);
+      else {
+        setError("An unexpected error occurred.");
+        console.log(result);
+      }
+      return;
+    }
+
+    const { data: authToken } = await loginApi.request(
+      userInfo.email,
+      userInfo.password
+    );
+    auth.login(authToken);
+  };
+
   return (
     <Screen style={styles.container}>
+      <AppActivityIndicator loading={registerApi.loading || loginApi.loading} />
       <Formik
         initialValues={{ name: "", email: "", password: "" }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}
       >
-        {({ handleChange, handlesubmit, errors }) => (
+        {({ handleChange, handleSubmit, errors }) => (
           <>
+            <ErrorMessage error={error} visible={error} />
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -34,6 +67,7 @@ const RegisterScreen = () => {
               autoCapitalize="none"
               autoCorrect={false}
               icon="email"
+              keyboardType="email-address"
               onChangeText={handleChange("email")}
               placeHolder="Email"
             />
@@ -43,9 +77,10 @@ const RegisterScreen = () => {
               autoCorrect={false}
               icon="lock"
               onChangeText={handleChange("password")}
+              secureTextEntry={true}
             />
             <ErrorMessage error={errors.password} />
-            <AppButton text="Register" onPress={handlesubmit} />
+            <AppButton text="Register" onPress={handleSubmit} />
           </>
         )}
       </Formik>
