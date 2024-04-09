72a62e6f467aeb496b6ba39ee3215c957acb00ce
SoundtrkSamurai <furtimat3@yahoo.com>
2024-04-08 19:06:34 -0700

    auth: Rewire auth into App component

--- App.js
+++ App.js
@@ -2,19 +2,18 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Button, Text, View } from "react-native";
-import "react-native-gesture-handler";
-import Screen from "./app/components/Screen";
-import AppNavigator from "./app/nagivation/AppNavigator";
-import navigationTheme from "./app/nagivation/navigationTheme";
-import OfflineNotice from "./app/components/OfflineNotice";
-import AuthNavigator from "./app/nagivation/AuthNavigator";
-import { useState, useEffect, useCallback } from "react";
-import AuthContext from "./app/auth/context";
-import authStorage from "./app/auth/storage";
-import { jwtDecode } from "jwt-decode";
 import "core-js/stable/atob";
 import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
+import OfflineNotice from "./app/components/OfflineNotice";
+import Screen from "./app/components/Screen";
+import AppNavigator from "./app/nagivation/AppNavigator";
+import AuthNavigator from "./app/nagivation/AuthNavigator";
+import navigationTheme from "./app/nagivation/navigationTheme";
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -83,20 +82,17 @@ export default function App() {
   const [user, setUser] = useState();
   const [isReady, setIsReady] = useState(false);
 
-  const restoreToken = async () => {
-    const token = await authStorage.getToken();
+  const restoreUser = async () => {
+    const authUser = await authStorage.getUser();
 
-    if (!token) {
+    if (authUser) {
+      setUser(authUser);
       setIsReady(true);
-      return;
     }
-
-    setUser(jwtDecode(token));
-    setIsReady(true);
   };
 
   useEffect(() => {
-    restoreToken();
+    restoreUser();
   }, []);
 
   const onLayoutRootView = useCallback(async () => {
